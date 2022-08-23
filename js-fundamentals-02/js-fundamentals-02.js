160319c44b3190cae24f319eddc0dbe7bcbbad9b
David Prieto <david.s.prieto1@gmail.com>
2022-08-22 19:44:19 -0500

    Completed Section 3 - #34

---
+++ js-fundamentals-02/js-fundamentals-02.js
@@ -0,0 +1,59 @@
+"use strict";
+
+/*
+// 08/22/22
+// Strict Mode:
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive.");
+
+// const interface = "Audio";
+// const private = 345;
+ */
+
+/*
+// Functions:
+function logger() {
+    console.log(`My name is David.`);
+}
+
+// calling / running / invoking function
+logger();
+
+function fruitProcessor(apples, oranges) {
+    return `Juice with ${apples} apples and ${oranges} oranges!`;
+}
+
+console.log(fruitProcessor(4, 5));
+// OR:
+const fruitJuice = fruitProcessor(4, 5);
+console.log(fruitJuice);
+
+
+// Function Declarations vs. Expressions:
+// function declaration (CAN be called before they're defined/initialized):
+calcAge1(); // function 'calcAge1' can be called before it is initialized because it is a function declaration.
+
+function calcAge1(birthYear, currentYear) {
+    return currentYear - birthYear;
+}
+
+console.log(calcAge1(1993, 2022));
+// OR:
+const age = calcAge1(1993, 2022);
+console.log(age);
+
+// function expression (a function saved in a variable; remember, expressions return values; CAN'T be called before they're defined/initialized):
+calcAge2(); // function 'calcAge2' can't be called before it is initialized because it is a function expression.
+
+const calcAge2 = function (birthYear, currentYear) {
+    return currentYear - birthYear;
+}
+
+console.log(calcAge2(1993, 2022));
+// OR:
+const age2 = calcAge2(1993, 2022);
+console.log(age2);
+ */
