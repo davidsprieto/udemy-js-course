99297873ba1d30ecf26f03688f7fd61945cb3e9c
David Prieto <david.s.prieto1@gmail.com>
2022-10-05 18:16:39 -0500

    Completed Section 3 - #38

--- js-03-fundamentals02/js-03-fundamentals02.js
+++ js-03-fundamentals02/js-03-fundamentals02.js
@@ -72,7 +72,8 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 }
 console.log(yearsUntilRetirement(1993, `David`));
 console.log(yearsUntilRetirement(1983, `Kelly`));
- */
+
+// Functions calling other functions:
 
 const cutFruitPieces = fruit => {return fruit * 4};
 
@@ -83,3 +84,29 @@ function fruitProcessor(apples, oranges) {
 }
 
 console.log(fruitProcessor(2, 3));
+ */
+
+// Coding Challenge #1:
+// See PDF for Challenge Details
+
+const calcAverage = (num1, num2, num3) => {return (num1 + num2 + num3) / 3}
+
+function checkWinner (avgDolphins, avgKoalas) {
+    if (avgDolphins > avgKoalas * 2) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas > avgDolphins * 2) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log("No winner");
+    }
+}
+
+// Test Data 1:
+const avgDolphins1 = calcAverage(44, 23, 71);
+const avgKoalas1 = calcAverage(65, 54, 49);
+checkWinner(avgDolphins1, avgKoalas1);
+
+// Test Data 2:
+const avgDolphins2 = calcAverage(85, 54, 41);
+const avgKoalas2 = calcAverage(23, 34, 27);
+checkWinner(avgDolphins2, avgKoalas2);
